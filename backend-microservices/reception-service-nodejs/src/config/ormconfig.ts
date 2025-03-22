3b9e87143972d3f83c4366cd7cafa65a1658ac7a
Jorge Contreras Prince <jorge.contreras@patagoniansys.com>
2025-03-21 19:32:03 -0500

    fix: add production settings for modules with typeorm

--- backend-microservices/reception-service-nodejs/src/config/ormconfig.ts
+++ backend-microservices/reception-service-nodejs/src/config/ormconfig.ts
@@ -1,12 +1,14 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: true, // Solo para desarrollo
   logging: false,
-  entities: ["src/entities/*.ts"],
+  entities: [isProd ? "dist/entities/*.js" : "src/entities/*.ts"],
 });
 
 export default AppDataSource;
